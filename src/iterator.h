ef5e34a74fdf6594744e4d21f3e2e1181a62e4e9
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-05-13 14:01:16 +0200

    Remove callback from iterator.
    Replace it with a boolean controlling whether recovery will
    be performed. It makes code easier to understand and prepares
    region API for further, timestamp-related changes.

--- src/iterator.h
+++ src/iterator.h
@@ -8,21 +8,20 @@
 
 #include "libpmemstream.h"
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef void (*region_runtime_initialize_fn_type)(struct pmemstream_region_runtime *, struct pmemstream_entry);
-
 struct pmemstream_entry_iterator {
+	/* Should this iterator perf region recovery when last valid entry is found. */
+	bool perform_recovery;
 	struct pmemstream *const stream;
 	const struct pmemstream_region region;
 	struct pmemstream_region_runtime *const region_runtime;
 	uint64_t offset;
-
-	region_runtime_initialize_fn_type region_runtime_initialize_fn;
 };
 
 struct pmemstream_region_iterator {
@@ -32,8 +31,7 @@ struct pmemstream_region_iterator {
 
 /* Initializes pmemstream_entry_iterator pointed to by 'iterator'. */
 int entry_iterator_initialize(struct pmemstream_entry_iterator *iterator, struct pmemstream *stream,
-			      struct pmemstream_region region,
-			      region_runtime_initialize_fn_type region_runtime_initialize_fn);
+			      struct pmemstream_region region, bool perform_recovery);
 
 #ifdef __cplusplus
 } /* end extern "C" */
